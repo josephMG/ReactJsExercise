19cb95e923150ac048b828274aa4d65da9852769
josephMG <joseph780116.mg@gmail.com>
2015-10-06 16:27:14 +0800

    Implementing the comment component

--- app/assets/javascripts/comments.js.jsx
+++ app/assets/javascripts/comments.js.jsx
@@ -1,16 +1,19 @@
-var HelloWorld = React.createClass({
-  render: function() {
+var Comment = React.createClass({
+  render: function () {
     return (
-      <div className='HelloWorld'>
-        Hello, world!
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.comment}
       </div>
-      );
+    );
   }
 });
 
 var ready = function () {
   React.render(
-    <HelloWorld />,
+    <Comment author="Richard" comment="This is a comment "/>,
     document.getElementById('comments')
   );
 };
